d847283ffc243ab501a7d5c41b8ba496eef361bd
Ortaz <Natz94@mail.ru>
2017-03-26 17:41:11 +0800

    Mycommit

---
+++ Main.java
@@ -0,0 +1,25 @@
+
+
+/**
+ *
+ * @author Михаил
+ */
+public class Main
+{
+
+    public static void main(String[] Args) {
+        System.out.println("Hello, i'm here!");
+        int a = 5;
+        int b = 8;
+        
+        System.out.println("a = " + a + "; b = " + b + "; summ = " + summ(a, b));
+        
+    }
+    
+    public static int summ(int a, int b)
+    {
+        return a + b;
+    }
+    
+}
+
